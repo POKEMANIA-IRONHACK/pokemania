76fa3433e9ad7dd434554e8c033dfff3066b56b1
Carl Berthold <Carl@space-time.tv>
2021-09-13 18:41:45 +0200

    display the data, the name

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User= require("../models/User.model")
 
+
 /* GET home page */
 router.get("/signup", (req, res, next) => {
   res.render("signup");
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,79 @@
 const router = require("express").Router();
+const Pokedex = require('pokedex-promise-v2');
+const P = new Pokedex();
+const axios = require("axios");
+
+//const pokeApi = new PokemonApi ({
+//  // clientId: process.env.CLIENT_ID,
+//  // clientSecret: process.env.CLIENT_SECRET
+//});
+
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
-});
+  res.render("index")
+})
+
+
+
+router.get("/main", (req,res,next) => {
+  axios
+  .get('https://pokeapi.co/api/v2/pokemon')
+  .then((response) => {
+  const x = response.data.results
+  console.log(response.data.results)
+  res.render("main",{pokemon: x});
+  })
+})
+
+
+
+
+
+
+
+
+
+
+//.then((response) => {
+//console.log(response.data.results);
+//res.render("/index", response.data)
+//}, (error) => {
+//  console.log(error);
+//});
+
+
+
+//   method:'Get',
+//   url:"https://pokeapi.co/api/v2",
+// })
+// .then((response) => {
+// console.log(response);
+// }, (error) => {
+//   console.log(error);
+// });
+  
+
+
+// router.get("/", (req, res, next) => {
+//   const pokemon = req.params.pokemon;
+//   //console.log(pokemon)
+//   P
+// .getPokemonByName('eevee')
+// .then((data) => { console.log("The received data from the API: ", data)})
+// res.render("index", data);
+// });
+
+
+
+
+//router.get("index", (req,res, next) => {
+//  const pokemon = req.params.pokemon;
+//  console.log(pokemon)
+//P
+//.getPokemonByName('eevee')
+//.then((data) => { console.log("The received data from the API: ", data)})
+//res.render("index", { data });
+//})
 
 module.exports = router;
