e492a6d44f339400ab63211b56fc858e82b4ab94
kumax0 <Canerunus@gmail.com>
2021-09-14 10:01:32 +0200

    cleaned up code

--- routes/auth.js
+++ routes/auth.js
@@ -16,16 +16,12 @@ router.get("/login", (req,res, next) => {
 router.post("/signup", (req, res, next) => {
 	console.log(req.body);
 	const { username, password } = req.body;
-	// create a new book in the database
 	User.create ({
 		username: username,
     password: password
 	})
 		.then(createdUser => {
 			console.log(createdUser);
-			// redirect / render the detail view for this book
-			// res.render('bookDetails', { book: createdBook });
-			// redirect to a specific url
 			res.redirect('/main');
 		})
 		.catch(err => next(err));
--- routes/index.js
+++ routes/index.js
@@ -3,77 +3,22 @@ const Pokedex = require('pokedex-promise-v2');
 const P = new Pokedex();
 const axios = require("axios");
 
-//const pokeApi = new PokemonApi ({
-//  // clientId: process.env.CLIENT_ID,
-//  // clientSecret: process.env.CLIENT_SECRET
-//});
-
-
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index")
 })
 
 
-
+// Get API data
 router.get("/main", (req,res,next) => {
   axios
   .get('https://pokeapi.co/api/v2/pokemon')
   .then((response) => {
   const x = response.data.results
   console.log(response.data.results)
+// Display Pokemon names
   res.render("main",{pokemon: x});
   })
 })
-
-
-
-
-
-
-
-
-
-
-//.then((response) => {
-//console.log(response.data.results);
-//res.render("/index", response.data)
-//}, (error) => {
-//  console.log(error);
-//});
-
-
-
-//   method:'Get',
-//   url:"https://pokeapi.co/api/v2",
-// })
-// .then((response) => {
-// console.log(response);
-// }, (error) => {
-//   console.log(error);
-// });
-  
-
-
-// router.get("/", (req, res, next) => {
-//   const pokemon = req.params.pokemon;
-//   //console.log(pokemon)
-//   P
-// .getPokemonByName('eevee')
-// .then((data) => { console.log("The received data from the API: ", data)})
-// res.render("index", data);
-// });
-
-
-
-
-//router.get("index", (req,res, next) => {
-//  const pokemon = req.params.pokemon;
-//  console.log(pokemon)
-//P
-//.getPokemonByName('eevee')
-//.then((data) => { console.log("The received data from the API: ", data)})
-//res.render("index", { data });
-//})
 
 module.exports = router;
